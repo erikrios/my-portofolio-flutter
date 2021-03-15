6a3734e887bf9da90fb4feed4a31336d205786a1
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-15 19:28:48 +0700

    Import home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'pages/home_page.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'Hello, World!',
-          textDirection: TextDirection.ltr,
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Erik Rio Setiawan',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
         ),
-      ),
-    );
-  }
+        home: HomePage(),
+      );
 }
