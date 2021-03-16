5a0a55cfe001dcadc1c1a918d436fae2d8f06cdd
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-16 20:43:11 +0700

    Create recent_work.dart

---
+++ lib/models/recent_work.dart
@@ -0,0 +1,12 @@
+class RecentWork {
+  final String name;
+  final String description;
+  final String url;
+  final String imagePath;
+
+  RecentWork(
+      {required this.name,
+      required this.description,
+      required this.url,
+      required this.imagePath});
+}
