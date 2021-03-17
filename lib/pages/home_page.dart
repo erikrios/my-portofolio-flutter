9aabf5454f4e3049108e245b977c2ef45e0f6ed0
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-17 13:14:29 +0700

    Set the responsive app bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,61 +4,76 @@ import 'package:my_portofolio_flutter/pages/sections/introduction.dart';
 import 'package:my_portofolio_flutter/pages/sections/recent_work.dart';
 import 'package:my_portofolio_flutter/pages/sections/resume.dart';
 import 'package:my_portofolio_flutter/pages/sections/specialization.dart';
+import 'package:my_portofolio_flutter/responsive/screen_size.dart';
+import 'package:my_portofolio_flutter/responsive/util.dart';
 
 import 'sections/footer.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 20),
-          child: Image.asset(
-            'assets/images/logo.png',
+    final Size _size = MediaQuery.of(context).size;
+    ScreenSize _screenSize;
+
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        switch (getScreenSize(constraints.maxWidth)) {
+          case ScreenSize.SMALL:
+            {
+              _screenSize = ScreenSize.SMALL;
+              print(ScreenSize.SMALL.toString());
+            }
+            break;
+          case ScreenSize.NORMAL:
+            {
+              _screenSize = ScreenSize.NORMAL;
+              print(ScreenSize.NORMAL.toString());
+            }
+            break;
+          case ScreenSize.MEDIUM:
+            {
+              _screenSize = ScreenSize.MEDIUM;
+              print(ScreenSize.MEDIUM.toString());
+            }
+            break;
+          case ScreenSize.LARGE:
+            {
+              _screenSize = ScreenSize.LARGE;
+              print(ScreenSize.LARGE.toString());
+            }
+        }
+        return Scaffold(
+          appBar: getAppBar(
+            screenSize: _screenSize,
           ),
-        ),
-        actions: [
-          getHeaderItem('Home'),
-          getHeaderItem('Blog'),
-          getHeaderItem('Contact'),
-          SizedBox(width: 100),
-        ],
-      ),
-      body: LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-          print('Max Width : ${constraints.maxWidth}');
-          print('Max Height : ${constraints.maxHeight}');
-          return Scrollbar(
+          body: Scrollbar(
             child: SingleChildScrollView(
               child: Column(
                 children: [
                   Introduction(
-                    height: size.height * 3 / 4,
+                    height: _size.height * 3 / 4,
                   ),
                   Resume(
-                    height: size.height * 3 / 4,
+                    height: _size.height * 3 / 4,
                   ),
                   Specialization(
-                    height: size.height * 3 / 4,
+                    height: _size.height * 3 / 4,
                   ),
                   RecentWork(
-                    height: size.height * 5 / 6,
+                    height: _size.height * 5 / 6,
                   ),
                   Contact(
-                    height: size.height * 5 / 6,
+                    height: _size.height * 5 / 6,
                   ),
                   Footer(
-                    height: size.height * 1 / 4,
+                    height: _size.height * 1 / 4,
                   ),
                 ],
               ),
             ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 
@@ -79,5 +94,28 @@ class HomePage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  AppBar? getAppBar({required ScreenSize screenSize}) {
+    bool isMediumOrLargeSize =
+        (screenSize == ScreenSize.MEDIUM || screenSize == ScreenSize.LARGE);
+    if (isMediumOrLargeSize) {
+      return AppBar(
+        backgroundColor: Colors.white,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: Image.asset(
+            'assets/images/logo.png',
+          ),
+        ),
+        actions: [
+          getHeaderItem('Home'),
+          getHeaderItem('Blog'),
+          getHeaderItem('Contact'),
+          SizedBox(width: 100),
+        ],
+      );
+    } else
+      return null;
   }
 }
