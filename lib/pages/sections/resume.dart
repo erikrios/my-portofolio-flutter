4d66e16060ec25014dcd40bb173627692a32b076
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-17 12:01:45 +0700

    Set max lines and text ellipsis

--- lib/pages/sections/resume.dart
+++ lib/pages/sections/resume.dart
@@ -50,6 +50,8 @@ class Resume extends StatelessWidget {
                   Text(
                     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                     textAlign: TextAlign.center,
+                    maxLines: 9,
+                    overflow: TextOverflow.ellipsis,
                     style: TextStyle(
                       fontSize: 16,
                       color: Colors.white,
