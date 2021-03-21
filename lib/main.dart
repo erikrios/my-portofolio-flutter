3150fde4d68a87b4ce43922bd37851af4256efe5
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-21 21:24:40 +0700

    Add a function to get portofolio

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:http/http.dart' as http;
 
 import 'pages/home_page.dart';
 
@@ -15,14 +16,21 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: 'Erik Rio Setiawan',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          textTheme: GoogleFonts.robotoTextTheme(
-            Theme.of(context).textTheme,
-          ),
+  Widget build(BuildContext context) {
+    getPortofolio().then((response) => print(response.body));
+    return MaterialApp(
+      title: 'Erik Rio Setiawan',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: GoogleFonts.robotoTextTheme(
+          Theme.of(context).textTheme,
         ),
-        home: HomePage(),
-      );
+      ),
+      home: HomePage(),
+    );
+  }
+
+  Future<http.Response> getPortofolio() {
+    return http.get(Uri.https('raw.githubusercontent.com', 'erikrios/dummy-file-testing/master/json/my-portofolio.json'));
+  }
 }
