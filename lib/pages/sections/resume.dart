021ea845054c147630e39c1265017aaab2f8b217
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-23 15:55:08 +0700

    Integrated the bloc builder

--- lib/pages/sections/resume.dart
+++ lib/pages/sections/resume.dart
@@ -1,6 +1,9 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_portofolio_flutter/bloc/portofolio/portofolio_bloc.dart';
+import 'package:my_portofolio_flutter/bloc/portofolio/portofolio_state.dart';
 import 'package:my_portofolio_flutter/responsive/screen_size.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -15,6 +18,7 @@ class Resume extends StatelessWidget {
     double width = MediaQuery.of(context).size.width;
     final bool isSmallOrNormalScreen =
         screenSize == ScreenSize.SMALL || screenSize == ScreenSize.NORMAL;
+    final PortofolioBloc bloc = BlocProvider.of<PortofolioBloc>(context);
 
     return Container(
       padding: EdgeInsets.all(
@@ -37,6 +41,7 @@ class Resume extends StatelessWidget {
                 Flexible(
                   flex: 3,
                   child: _handleInfo(
+                    bloc: bloc,
                     isSmallOrNormalScreen: isSmallOrNormalScreen,
                   ),
                 ),
@@ -54,6 +59,7 @@ class Resume extends StatelessWidget {
                 Flexible(
                   flex: 1,
                   child: _handleInfo(
+                    bloc: bloc,
                     isSmallOrNormalScreen: isSmallOrNormalScreen,
                   ),
                 ),
@@ -78,71 +84,86 @@ class Resume extends StatelessWidget {
     );
   }
 
-  Widget _handleInfo({required bool isSmallOrNormalScreen}) {
-    return Container(
-      padding: isSmallOrNormalScreen
-          ? EdgeInsets.all(16.0)
-          : EdgeInsets.only(right: 100, top: 50, bottom: 50),
-      child: Column(
-        mainAxisAlignment: isSmallOrNormalScreen
-            ? MainAxisAlignment.spaceBetween
-            : MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            'Hi, Nice to meet you.',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: isSmallOrNormalScreen ? 18.0 : 26.0,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          Text(
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            textAlign: TextAlign.center,
-            maxLines: 9,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              fontSize: isSmallOrNormalScreen ? 14.0 : 16.0,
-              color: Colors.white,
-            ),
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              final String url = 'https://www.linkedin.com/in/erikriosetiawan/';
-              await canLaunch(url)
-                  ? await launch(url)
-                  : throw 'Could not launch $url';
-            },
-            child: Text('Download CV'),
-            style: ButtonStyle(
-              padding: MaterialStateProperty.all(
-                isSmallOrNormalScreen
-                    ? EdgeInsets.symmetric(
-                        vertical: 12.0,
-                        horizontal: 18.0,
-                      )
-                    : EdgeInsets.symmetric(
-                        vertical: 14.0,
-                        horizontal: 24.0,
-                      ),
+  Widget _handleInfo(
+      {required PortofolioBloc bloc, required bool isSmallOrNormalScreen}) {
+    return BlocBuilder<PortofolioBloc, PortofolioState>(
+      bloc: bloc,
+      builder: (context, state) => Container(
+        padding: isSmallOrNormalScreen
+            ? EdgeInsets.all(16.0)
+            : EdgeInsets.only(right: 100, top: 50, bottom: 50),
+        child: Column(
+          mainAxisAlignment: isSmallOrNormalScreen
+              ? MainAxisAlignment.spaceBetween
+              : MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Hi, Nice to meet you.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: isSmallOrNormalScreen ? 18.0 : 26.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
               ),
-              textStyle: MaterialStateProperty.all(
-                TextStyle(
-                  fontSize: isSmallOrNormalScreen ? 16.0 : 18.0,
-                  color: Colors.blue.shade700,
+            ),
+            Text(
+              state is PortofolioLoadingState
+                  ? "Loading..."
+                  : state is PortofolioErrorState
+                      ? "No Data"
+                      : (state as PortofolioSuccessState).portofolio.resume,
+              textAlign: TextAlign.center,
+              maxLines: 9,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                fontSize: isSmallOrNormalScreen ? 14.0 : 16.0,
+                color: Colors.white,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                final String url = state is PortofolioSuccessState
+                    ? state.portofolio.contacts
+                        .singleWhere((contact) =>
+                            contact.platform.toLowerCase() ==
+                            'LinkedIn'.toLowerCase())
+                        .url
+                    : 'https://www.linkedin.com/in/erikriosetiawan/';
+                await canLaunch(url)
+                    ? await launch(url)
+                    : throw 'Could not launch $url';
+              },
+              child: Text('Download CV'),
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all(
+                  isSmallOrNormalScreen
+                      ? EdgeInsets.symmetric(
+                          vertical: 12.0,
+                          horizontal: 18.0,
+                        )
+                      : EdgeInsets.symmetric(
+                          vertical: 14.0,
+                          horizontal: 24.0,
+                        ),
+                ),
+                textStyle: MaterialStateProperty.all(
+                  TextStyle(
+                    fontSize: isSmallOrNormalScreen ? 16.0 : 18.0,
+                    color: Colors.blue.shade700,
+                  ),
+                ),
+                backgroundColor:
+                    MaterialStateProperty.all(Colors.amber.shade800),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18.0),
+                  ),
                 ),
               ),
-              backgroundColor: MaterialStateProperty.all(Colors.amber.shade800),
-              shape: MaterialStateProperty.all(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0),
-                ),
-              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
