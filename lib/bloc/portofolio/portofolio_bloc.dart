1993d797344a58d4deeb3ac2c751f17d47cc0855
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-23 14:33:01 +0700

    Create portofolio_bloc.dart

---
+++ lib/bloc/portofolio/portofolio_bloc.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
+import 'package:my_portofolio_flutter/bloc/portofolio/portofolio_event.dart';
+import 'package:my_portofolio_flutter/bloc/portofolio/portofolio_state.dart';
+import 'package:my_portofolio_flutter/models/portofolio.dart';
+
+class PortofolioBloc extends Bloc<PortofolioEvent, PortofolioState> {
+  PortofolioBloc() : super(PortofolioLoadingState());
+
+  @override
+  Stream<PortofolioState> mapEventToState(PortofolioEvent event) async* {
+    switch (event) {
+      case PortofolioEvent.GET_DATA:
+        {
+          yield PortofolioLoadingState();
+          try {
+            Portofolio portofolio = await getPortofolio();
+            yield PortofolioSuccessState(portofolio: portofolio);
+          } on Exception catch (e) {
+            yield PortofolioErrorState(exception: e);
+          }
+        }
+        break;
+    }
+  }
+
+  Future<Portofolio> getPortofolio() async {
+    http.Response response = await http.get(Uri.https(
+        'raw.githubusercontent.com',
+        'erikrios/dummy-file-testing/master/json/my-portofolio.json'));
+    if (response.statusCode == 200)
+      return Portofolio.fromJson(jsonDecode(response.body));
+    else
+      throw Exception('Failed to load portofolio');
+  }
+}
