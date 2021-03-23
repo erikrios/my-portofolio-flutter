0ba317813eb57e7308577a47defaf8ad045739ee
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-23 14:39:30 +0700

    Remove unnecessary code

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'dart:convert';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:http/http.dart' as http;
-import 'package:my_portofolio_flutter/models/portofolio.dart';
 
 import 'pages/home_page.dart';
 
@@ -20,7 +16,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    getPortofolio().then((portofolio) => print(portofolio));
     return MaterialApp(
       title: 'Erik Rio Setiawan',
       theme: ThemeData(
@@ -31,15 +26,5 @@ class MyApp extends StatelessWidget {
       ),
       home: HomePage(),
     );
-  }
-
-  Future<Portofolio> getPortofolio() async {
-    http.Response response = await http.get(Uri.https(
-        'raw.githubusercontent.com',
-        'erikrios/dummy-file-testing/master/json/my-portofolio.json'));
-    if (response.statusCode == 200)
-      return Portofolio.fromJson(jsonDecode(response.body));
-    else
-      throw Exception('Failed to load portofolio');
   }
 }
