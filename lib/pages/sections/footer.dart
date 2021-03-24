2de3a0eaa5436af88d4d85dd4a941879cac4a69b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2021-03-24 09:51:38 +0700

    Integrated the bloc builder

--- lib/pages/sections/footer.dart
+++ lib/pages/sections/footer.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_portofolio_flutter/bloc/portofolio/portofolio_bloc.dart';
+import 'package:my_portofolio_flutter/bloc/portofolio/portofolio_state.dart';
 import 'package:my_portofolio_flutter/responsive/screen_size.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -12,6 +15,8 @@ class Footer extends StatelessWidget {
   Widget build(BuildContext context) {
     bool isSmallOrNormalScreen =
         screenSize == ScreenSize.SMALL || screenSize == ScreenSize.NORMAL;
+    final bloc = BlocProvider.of<PortofolioBloc>(context);
+
     return Container(
       height: isSmallOrNormalScreen ? height * 1 / 6 : height * 1 / 4,
       color: Colors.grey.shade200,
@@ -19,46 +24,30 @@ class Footer extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              _socialIcon(
-                  path: 'assets/images/linkedin.png',
-                  iconSize: isSmallOrNormalScreen ? 24.0 : 36.0,
-                  padding: isSmallOrNormalScreen ? 4.0 : 8.0,
-                  onPressed: () async {
-                    final String url =
-                        'https://www.linkedin.com/in/erikriosetiawan/';
-                    await canLaunch(url)
-                        ? await launch(url)
-                        : throw 'Could not launch $url';
-                  }),
-              _socialIcon(
-                  path: 'assets/images/github.png',
-                  iconSize: isSmallOrNormalScreen ? 24.0 : 36.0,
-                  padding: isSmallOrNormalScreen ? 4.0 : 8.0,
-                  onPressed: () async {
-                    final String url = 'https://github.com/erikrios';
-                    await canLaunch(url)
-                        ? await launch(url)
-                        : throw 'Could not launch $url';
-                  }),
-              _socialIcon(
-                  path: 'assets/images/email.png',
-                  iconSize: isSmallOrNormalScreen ? 24.0 : 36.0,
-                  padding: isSmallOrNormalScreen ? 4.0 : 8.0,
-                  onPressed: () async {
-                    final String url = 'mailto:erikriosetiawan15@gmail.com';
-                    await canLaunch(url)
-                        ? await launch(url)
-                        : throw 'Could not launch $url';
-                  }),
-            ],
+          BlocBuilder<PortofolioBloc, PortofolioState>(
+            bloc: bloc,
+            builder: (context, state) => Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: state is PortofolioSuccessState
+                  ? state.portofolio.contacts
+                      .map((contact) => _socialIcon(
+                          path: contact.imagePath,
+                          iconSize: isSmallOrNormalScreen ? 24.0 : 36.0,
+                          padding: isSmallOrNormalScreen ? 4.0 : 8.0,
+                          onPressed: () async {
+                            final String url = contact.url;
+                            await canLaunch(url)
+                                ? await launch(url)
+                                : throw 'Could not launch $url';
+                          }))
+                      .toList()
+                  : [],
+            ),
           ),
           Text(
             isSmallOrNormalScreen
-                ? '\u00a9 All Right Reserved\nDesigned by Erik Rio Setiwan'
-                : '\u00a9 All Right Reserved - Designed by Erik Rio Setiwan',
+                ? '\u00a9 All Right Reserved\nDesigned by Erik Rio Setiawan'
+                : '\u00a9 All Right Reserved - Designed by Erik Rio Setiawan',
             textAlign: TextAlign.center,
             style: TextStyle(
               color: Colors.blue.shade700,
